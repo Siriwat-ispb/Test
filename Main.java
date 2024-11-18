0a1f419de4bf0a272948cdeb6ea80502def46bc5
Siriwat Ittisompiboon <159911859+Siriwat-ispb@users.noreply.github.com>
2024-11-18 03:42:22 +0000

    WW

--- Main.java
+++ Main.java
@@ -11,7 +11,7 @@ class ActorMap {
     private LinkedHashSet<String> resultSet;
     private HashSet<String> totalActors;
 
-    public ActorMap() {     // Constructor
+    public ActorMap() {    
         workingMap = new TreeMap<>();
         resultSet = new LinkedHashSet<>();
         totalActors = new HashSet<>();
@@ -26,11 +26,11 @@ class ActorMap {
         }
     }
 
-    public void checkActor(HashSet<String> chosenActor) {   // Find same name/surname with different surname/name
+    public void checkActor(HashSet<String> chosenActor) {
         totalActors.clear();
         resultSet.clear();
 
-        for (String input : chosenActor) {      // Check actor name
+        for (String input : chosenActor) { 
             for (String actor : workingMap.keySet()) {
                 if (actor.toLowerCase().contains(input.toLowerCase())) {
                     totalActors.add(actor);
@@ -40,16 +40,16 @@ class ActorMap {
     }
 
     public void initialActors(HashSet<String> chosenActor) {
-        checkActor(chosenActor);    // <--- this function already done it all
+        checkActor(chosenActor); 
 
         HashSet<String> totalMovies = new HashSet<>();
         System.out.println("Valid input actors = " + totalActors + "\n");
 
         for (String actor : totalActors) { 
             LinkedHashSet<String> movies = workingMap.get(actor);
-            totalMovies.addAll(movies);     // movies count
+            totalMovies.addAll(movies); 
 
-            System.out.printf("%20s", actor);   // print showcase
+            System.out.printf("%20s", actor);
             for (String movie : movies) {
                 System.out.printf("  >>  %-25s", movie);
             }
@@ -60,24 +60,24 @@ class ActorMap {
     }
 
     public void containActors(HashSet<String> chosenActor) {
-        checkActor(chosenActor);    // Function
+        checkActor(chosenActor);
 
         System.out.println("Valid input actors = " + totalActors + "\n");
     
         boolean isFirstActor = true;
-        for (String actor : totalActors) {  // actor from result
+        for (String actor : totalActors) { 
             LinkedHashSet<String> movies = workingMap.get(actor);
 
-            System.out.printf("%20s", actor);   // print showcase
+            System.out.printf("%20s", actor);
             for (String movie : movies) {
                 System.out.printf("  >>  %-25s", movie);
             }
             System.out.println();
 
-            if (isFirstActor) {     // Assign resultSet a data of actor's movie itself on first time
+            if (isFirstActor) {  
                 resultSet.addAll(movies);
                 isFirstActor = false;
-            } else resultSet.retainAll(movies);     // Keep all repeated movies
+            } else resultSet.retainAll(movies); 
         }
 
         System.out.println("\nResult = " + resultSet);
@@ -85,31 +85,31 @@ class ActorMap {
     }
      
     public void withoutActors(HashSet<String> chosenActor) {
-        checkActor(chosenActor);    // Function
+        checkActor(chosenActor);  
 
         System.out.println("Valid input actors = " + totalActors + "\n");
-        for (LinkedHashSet<String> movies : workingMap.values()) resultSet.addAll(movies);  // Assign resultSet every existing movies
+        for (LinkedHashSet<String> movies : workingMap.values()) resultSet.addAll(movies);  
 
-        for (String actor : totalActors) {  // actor from result
+        for (String actor : totalActors) { 
             LinkedHashSet<String> movies = workingMap.get(actor);
 
-            System.out.printf("%20s", actor);      // Print showcase
+            System.out.printf("%20s", actor);   
             for (String movie : movies) {
                 System.out.printf("  >>  %-25s", movie);
             }
             System.out.println();
 
-            resultSet.removeAll(movies);    // remove movies with input actor's movie
+            resultSet.removeAll(movies);   
         }
 
-        System.out.println("\nResult = " + resultSet);      // print all movie result
-        System.out.printf("\nTotal movies = %d\n", resultSet.size());   // print total movie result
+        System.out.println("\nResult = " + resultSet);  
+        System.out.printf("\nTotal movies = %d\n", resultSet.size());  
     }
 
-    public void printWorkingMap() {     // Initial map
-        HashSet<String> totalMovies = new HashSet<>();  // Store movie excluding duplicate, purpose for printing movie count
+    public void printWorkingMap() {     
+        HashSet<String> totalMovies = new HashSet<>();  
 
-        for (Map.Entry<String, LinkedHashSet<String>> entry : workingMap.entrySet()) {  // Check whole workingMap for actor and movies
+        for (Map.Entry<String, LinkedHashSet<String>> entry : workingMap.entrySet()) { 
             String actor = entry.getKey();
             LinkedHashSet<String> movies = entry.getValue();
 
@@ -130,14 +130,13 @@ public class Main {
 
         try {
             Scanner fileScanner = new Scanner(new File("/workspaces/Test/movies.txt"));
-            // Scanner fileScanner = new Scanner(new File("/src/main/java/Ex8_6581167/movies.txt"));
-            while (fileScanner.hasNextLine()) {         // Check every existing line
+            while (fileScanner.hasNextLine()) {      
                 String line = fileScanner.nextLine();
                 String[] data = line.split(";");
                 
                 String movie = data[0].trim();
                 HashSet<String> actors = new HashSet<>();
-                for (int i=1; i < data.length; i++) {   // Check every ';' of that line
+                for (int i=1; i < data.length; i++) {  
                     actors.add(data[i].trim());
                 }
                 actorMap.addMovieActor(movie, actors);
@@ -145,10 +144,10 @@ public class Main {
             fileScanner.close();
         } catch (FileNotFoundException e) { e.printStackTrace(); }
 
-        actorMap.printWorkingMap();     // Print initial map
+        actorMap.printWorkingMap();     
 
         Scanner scanner = new Scanner(System.in);
-        while (true) {      // Loop ask for input
+        while (true) {      
             if (!askInput(actorMap, scanner)) {
                 System.out.println("Exiting Program...");
                 break;
@@ -166,13 +165,13 @@ public class Main {
         System.out.println("           Other = Quit");
 
         try {
-            choice = Integer.parseInt(scanner.nextLine());  // Select choice
+            choice = Integer.parseInt(scanner.nextLine());
         } catch (NumberFormatException e) { choice = -1; }
 
-        if (choice < 0 || choice > 2) { return false; } // Other: exit program
+        if (choice < 0 || choice > 2) { return false; }
 
         HashSet<String> chosenActor = new HashSet<>();
-        System.out.printf("Add names or surnames separated by comma ',' = ");   // Insert actor name > HashSet
+        System.out.printf("Add names or surnames separated by comma ',' = ");
         String[] actorInput = (scanner.nextLine()).split(",");
         for (int i=0; i < actorInput.length; i++) {
             chosenActor.add(actorInput[i].trim());
@@ -184,6 +183,6 @@ public class Main {
             case 2: actorMap.withoutActors(chosenActor); break;
             default: break;
         }
-        return true;    // Continue Loop back at main
+        return true;
     }
 }
