41eb51133469d301b67fe7a752e767ff0bfe7831
Siriwat Ittisompiboon <159911859+Siriwat-ispb@users.noreply.github.com>
2024-11-18 05:21:17 +0000

    new

--- Main.java
+++ Main.java
@@ -8,14 +8,12 @@ import java.util.*;
 class ActorMap { 
     // Key = actor, Value = set of movies 
     // Java's String already has consistent equals & compareTo for equality check 
-    private TreeMap<String, LinkedHashSet<String>> workingMap;  // Movie(value) , Actors(key)
+    private TreeMap<String, LinkedHashSet<String>> workingMap; 
     private LinkedHashSet<String> resultSet;
-    private HashSet<String> totalActors;
 
-    public ActorMap() {    
+    public ActorMap() {   
         workingMap = new TreeMap<>();
         resultSet = new LinkedHashSet<>();
-        totalActors = new HashSet<>();
     }
 
     public void addMovieActor(String movie, HashSet<String> actors) {
@@ -27,26 +25,20 @@ class ActorMap {
         }
     }
 
-    public void checkActor(HashSet<String> chosenActor) {
-        totalActors.clear();
-        resultSet.clear();
-
-        for (String input : chosenActor) { 
+    public void initialActors(HashSet<String> chosenActor) {
+        resultSet.clear();     
+        for (String input : chosenActor) {   
             for (String actor : workingMap.keySet()) {
                 if (actor.toLowerCase().contains(input.toLowerCase())) {
-                    totalActors.add(actor);
+                    resultSet.add(actor);
                 }
             }
         }
-    }
-
-    public void initialActors(HashSet<String> chosenActor) {
-        checkActor(chosenActor); 
 
         HashSet<String> totalMovies = new HashSet<>();
-        System.out.println("Valid input actors = " + totalActors + "\n");
+        System.out.println("Valid input actors = " + resultSet + "\n");
 
-        for (String actor : totalActors) { 
+        for (String actor : resultSet) { 
             LinkedHashSet<String> movies = workingMap.get(actor);
             totalMovies.addAll(movies); 
 
@@ -61,54 +53,70 @@ class ActorMap {
     }
 
     public void containActors(HashSet<String> chosenActor) {
-        checkActor(chosenActor);
 
-        System.out.println("Valid input actors = " + totalActors + "\n");
-    
+        HashSet<String> TActors = new HashSet<>();
+        for (String input : chosenActor) {
+            for (String actor : resultSet) {
+                if (actor.toLowerCase().contains(input.toLowerCase())) {
+                TActors.add(actor);
+                }
+            }
+        }
+
         boolean isFirstActor = true;
-        for (String actor : totalActors) { 
+        HashSet<String> TMovies = new HashSet<>();
+        System.out.println("Valid input actors = " + TActors + "\n");
+        for (String actor : TActors) { 
             LinkedHashSet<String> movies = workingMap.get(actor);
 
-            System.out.printf("%20s", actor);
+            System.out.printf("%20s", actor); 
             for (String movie : movies) {
                 System.out.printf("  >>  %-25s", movie);
             }
             System.out.println();
 
-            if (isFirstActor) {  
-                resultSet.addAll(movies);
+            if (isFirstActor) {   
+                TMovies.addAll(movies);
                 isFirstActor = false;
-            } else resultSet.retainAll(movies); 
+            } else TMovies.retainAll(movies);
         }
 
-        System.out.println("\nResult = " + resultSet);
-        System.out.printf("\nTotal movies = %d\n", resultSet.size());
+        System.out.println("\nResult = " + TMovies);
+        System.out.printf("\nTotal movies = %d\n", TMovies.size());
     }
      
     public void withoutActors(HashSet<String> chosenActor) {
-        checkActor(chosenActor);  
 
-        System.out.println("Valid input actors = " + totalActors + "\n");
-        for (LinkedHashSet<String> movies : workingMap.values()) resultSet.addAll(movies);  
+        HashSet<String> TActors = new HashSet<>();
+        HashSet<String> TMovies = new HashSet<>();
+        for (String input : chosenActor) {
+            for (String actor : resultSet) {
+                TMovies.addAll(workingMap.get(actor));
+                if (actor.toLowerCase().contains(input.toLowerCase())) {
+                TActors.add(actor);
+                }
+            }
+        }
+        System.out.println("Valid input actors = " + TActors + "\n");
 
-        for (String actor : totalActors) { 
+        for (String actor : TActors) { 
             LinkedHashSet<String> movies = workingMap.get(actor);
 
-            System.out.printf("%20s", actor);   
+            System.out.printf("%20s", actor);    
             for (String movie : movies) {
                 System.out.printf("  >>  %-25s", movie);
             }
             System.out.println();
 
-            resultSet.removeAll(movies);   
+            TMovies.removeAll(movies);  
         }
 
-        System.out.println("\nResult = " + resultSet);  
-        System.out.printf("\nTotal movies = %d\n", resultSet.size());  
+        System.out.println("\nResult = " + TMovies);   
+        System.out.printf("\nTotal movies = %d\n", TMovies.size());   
     }
 
     public void printWorkingMap() {     
-        HashSet<String> totalMovies = new HashSet<>();  
+        HashSet<String> totalMovies = new HashSet<>();
 
         for (Map.Entry<String, LinkedHashSet<String>> entry : workingMap.entrySet()) { 
             String actor = entry.getKey();
@@ -122,8 +130,7 @@ class ActorMap {
         }
         System.out.printf("\n\nTotal Movies = %d\nTotal Actors = %d\n", totalMovies.size(), workingMap.size());
     }
-} 
-
+}
 
 public class Main {
     public static void main(String args[]) {
@@ -131,13 +138,13 @@ public class Main {
 
         try {
             Scanner fileScanner = new Scanner(new File("/src/main/java/Ex8_6581098/movies.txt"));
-            while (fileScanner.hasNextLine()) {      
+            while (fileScanner.hasNextLine()) {         
                 String line = fileScanner.nextLine();
                 String[] data = line.split(";");
                 
                 String movie = data[0].trim();
                 HashSet<String> actors = new HashSet<>();
-                for (int i=1; i < data.length; i++) {  
+                for (int i=1; i < data.length; i++) {   
                     actors.add(data[i].trim());
                 }
                 actorMap.addMovieActor(movie, actors);
@@ -145,10 +152,10 @@ public class Main {
             fileScanner.close();
         } catch (FileNotFoundException e) { e.printStackTrace(); }
 
-        actorMap.printWorkingMap();     
+        actorMap.printWorkingMap();    
 
         Scanner scanner = new Scanner(System.in);
-        while (true) {      
+        while (true) {     
             if (!askInput(actorMap, scanner)) {
                 System.out.println("Exiting Program...");
                 break;
@@ -163,16 +170,16 @@ public class Main {
         System.out.println("Find movies >> 0 = Set initial actors");
         System.out.println("               1 = Contain actors");
         System.out.println("               2 = Without actors");
-        System.out.println("           Other = Quit");
+        System.out.println("               Other = Quit");
 
         try {
-            choice = Integer.parseInt(scanner.nextLine());
+            choice = Integer.parseInt(scanner.nextLine()); 
         } catch (NumberFormatException e) { choice = -1; }
 
-        if (choice < 0 || choice > 2) { return false; }
+        if (choice < 0 || choice > 2) { return false; } 
 
         HashSet<String> chosenActor = new HashSet<>();
-        System.out.printf("Add names or surnames separated by comma ',' = ");
+        System.out.printf("Add names or surnames separated by comma ',' = ");  
         String[] actorInput = (scanner.nextLine()).split(",");
         for (int i=0; i < actorInput.length; i++) {
             chosenActor.add(actorInput[i].trim());
@@ -184,6 +191,6 @@ public class Main {
             case 2: actorMap.withoutActors(chosenActor); break;
             default: break;
         }
-        return true;
+        return true;  
     }
 }
