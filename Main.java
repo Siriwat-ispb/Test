8ab0140143f967015b64d2089ddf42c295aa7486
Siriwat Ittisompiboon <159911859+Siriwat-ispb@users.noreply.github.com>
2025-01-09 07:59:09 +0000

    update

--- Main.java
+++ Main.java
@@ -1,217 +1,54 @@
-// package Ex9_6581167;
+// Phukij 6581096 , Siriwat 6581098
+import java.util.Scanner;
 
-// Siriwat Ittisompiboon 6581098
-
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.*;
-import org.jgrapht.*;
-import org.jgrapht.graph.*;
-import org.jgrapht.alg.color.*;
-
-
-
-public class Main {
-    public static void main(String args[]) {
-        Scanner scanner = new Scanner(System.in);
-        ActorGraph actorGraph = new ActorGraph();
-        String filePath = "/src/main/java/Ex9_6581098/movies.txt";
-
-        actorGraph.buildGraph(filePath);
-        actorGraph.baconParties();
-
-        while (true) {
-            if (!askInput(actorGraph, scanner)) {
-                System.out.println("Exiting Program...");
+public class Main{
+    
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        int Hour,Min,EndHour,EndMin;
+        System.out.println("=== Start time ===");
+        while(true){
+            System.out.println("Enter hour digit (0 - 23) = ");
+            Hour = input.nextInt();
+            if (Hour >=0 && Hour <=23){
                 break;
             }
         }
-        scanner.close();
-    }
-
-    public static boolean askInput(ActorGraph actorGraph, Scanner scanner) {
-        System.out.println("Enter name or surname, or 0 to quit");
-        String userInput[] = scanner.nextLine().split(",");
-          
-        if (userInput[0].contains("0")) return false;
-
-        HashSet<String> chosenActor = new HashSet<>();
-        for (int i=0; i<userInput.length; i++) {
-            chosenActor.add(userInput[i].trim());
-        }
-
-        actorGraph.findActor(chosenActor);
-        actorGraph.baconDegree();
-        return true;
-    }
-}
-
-
-
-class ActorGraph {
-    private Graph<String, DefaultEdge> costarGraph;                
-    private GreedyColoring<String, DefaultEdge> conflictGraph;    
-
-    private TreeMap<String, LinkedHashSet<String>> workingMap;   
-    private LinkedHashSet<String> resultSet;                        
-
-    public static final String BACON = "Kevin Bacon";
-
-    public ActorGraph() { 
-        this.costarGraph = new SimpleGraph<>(DefaultEdge.class);
-        this.workingMap = new TreeMap<>();
-        this.resultSet = new LinkedHashSet<>();
-    }
-
-    public void buildGraph(String fileName) {
-        try {
-            Scanner fileScanner = new Scanner(new File(fileName));
-
-            while (fileScanner.hasNextLine()) {         
-                String line = fileScanner.nextLine().trim();
-                String[] data = line.split(";");  
-                String movie = data[0];
-
-                HashSet<String> actors = new HashSet<>();  
-                for (int i=1; i < data.length; i++) {
-                    actors.add(data[i]);
-                }
-                addMovieActor(movie, actors);
-               
-                for (int i=1; i<data.length; i++) {   
-                    costarGraph.addVertex(data[i]);
-                    for (int j=1; j<i; j++) {
-                        costarGraph.addEdge(data[i], data[j]);
-                    }
-                }
-            }
-            Graph<String, DefaultEdge> tempGraph = new SimpleGraph<>(DefaultEdge.class); 
-            for (String vertex : costarGraph.vertexSet()) {    
-                tempGraph.addVertex(vertex);
-            }
-            for (DefaultEdge edge : costarGraph.edgeSet()) {   
-                String source = costarGraph.getEdgeSource(edge);
-                String target = costarGraph.getEdgeTarget(edge);
-                tempGraph.addEdge(source, target);
-            }
-            tempGraph.removeVertex(BACON);                     
-
-            conflictGraph = new GreedyColoring<>(tempGraph);   
-            fileScanner.close();
-        }
-        catch (FileNotFoundException e){
-            System.out.printf("Error: File not found (%s)\n", fileName);
-            e.printStackTrace();
-        }
-    }
-
-    public void addMovieActor(String movie, HashSet<String> actors) {
-        for (String actor : actors) {
-            if (!workingMap.containsKey(actor)) {
-                workingMap.put(actor, new LinkedHashSet<>());
-            }
-            workingMap.get(actor).add(movie);
-        }
-    }
-
-    public void findActor(HashSet<String> chosenActor) {
-        resultSet.clear();
-        for (String input : chosenActor) {   
-            for (String actor : workingMap.keySet()) {
-                if (actor.toLowerCase().contains(input.toLowerCase())) {
-                    resultSet.add(actor);
-                }
+        while(true){
+            System.out.println("Enter minute digit (0 - 59) = ");
+            Min = input.nextInt();
+            if (Min >=0 && Min <=59){
+                break;
             }
         }
-    }
 
-    public void baconDegree() {  
-        if (resultSet.isEmpty()) {
-            System.out.println("Actor not found");
-            return;
-        }
-        System.out.println("============================== Bacon degrees ==============================");
-        System.out.println("Valid actors = " + resultSet);
-        System.out.println();
-        for (String actor : resultSet) {
-            String start = actor;
-            String target = BACON;
-
-            LinkedList<String> queue = new LinkedList<>();
-            queue.add(start);
-
-            Set<String> visited = new HashSet<>();
-            visited.add(start);
-
-            Map<String, String> parentMap = new HashMap<>();
-            boolean found = false;
-
-            while (!queue.isEmpty() && !found) {
-                String current = queue.poll();
-
-                for (DefaultEdge edge : costarGraph.edgesOf(current)) {
-                    String neighbor = costarGraph.getEdgeSource(edge).equals(current)
-                        ? costarGraph.getEdgeTarget(edge)
-                        : costarGraph.getEdgeSource(edge);
-                    
-                    if (!visited.contains(neighbor)) {
-                        visited.add(neighbor);
-                        parentMap.put(neighbor, current);
-                        queue.add(neighbor);
-
-                        if (neighbor.equals(target)) {  
-                            found = true;
-                            break;
-                        }
-                    }
-                }
-            }
-
-            List<String> path = new LinkedList<>();
-            if (found) {
-                String current = target;
-                while (current != null) {  
-                    path.add(0, current);
-                    current = parentMap.get(current);
-                }
-                System.out.printf("%s  >>  Bacon degree = %d\n\n", start, path.size()-1);
-                for (int i=0; i<path.size()-1; i++) {
-                    String actor1 = path.get(i);
-                    String actor2 = path.get(i+1);
-
-                    Set<String> movieActor1 = workingMap.get(actor1);
-                    Set<String> movieActor2 = workingMap.get(actor2);
-
-                    Set<String> sharedMovie = new HashSet<>(movieActor1);
-                    sharedMovie.retainAll(movieActor2);
-
-                    System.out.printf("%20s - %-20s %s\n", actor1, actor2, sharedMovie);
-                }
-                System.out.println();
-            } else {
-                System.out.println("Connection not found");
+        System.out.println("\n=== End time ===");
+        while(true){
+            System.out.println("Enter hour digit (0 - 23) = ");
+            EndHour = input.nextInt();
+            if (EndHour >=0 && EndHour <=23){
+                break;
             }
         }
-        System.out.println("============================== Bacon degrees ==============================");
-    }
-
-    public void baconParties() {
-        List<Set<String>> colorList = conflictGraph.getColoring().getColorClasses();
-
-        System.out.println("\n\n============================== Bacon parties ==============================");
-        System.out.println("By GreedyColoring  >>  total parties = " + colorList.size());
-        System.out.println();
-        for (int i=0; i<colorList.size();i++) {
-            System.out.printf("Parties %d >> guests = %d", i+1, colorList.get(i).size());
-            Set<String> colorClass = colorList.get(i);
-            int count = 0;
-            for (String actor : colorClass) {
-                if (count % 6 == 0) System.out.println(); 
-                System.out.printf("%-20s", actor);
-                count++;
+        while(true){
+            System.out.println("Enter minute digit (0 - 59) = ");
+            EndMin = input.nextInt();
+            if (EndMin >=0 && EndMin <=59){
+                break;
             }
-            System.out.printf("\n\n");
         }
-        System.out.println("============================== Bacon parties ==============================");
+
+        System.out.printf("start time = %02d:%02d, End time = %02d:%02d ",Hour,Min,EndHour,EndMin);
+        if(EndHour==Hour && EndMin<Min || EndHour<Hour){System.out.println("(tomorrow)");}
+        int FinalHour,FinalMin;
+        FinalHour = EndHour-Hour;
+        if (FinalHour < 0){FinalHour+=24;}
+        FinalMin = EndMin-Min;
+        if(FinalMin<0){FinalMin+=60; FinalHour-=1;}
+
+        System.out.printf("Duration = %1d hours, %1d minutes\n",FinalHour,FinalMin);
+
+        //System.out.println("Enter hour digit (0 - 59) = ");
+        //int Min = input.nextInt();
     }
 }
