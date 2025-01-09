008bb66badcaf795a9379174ed930fa6e72b2b58
Siriwat Ittisompiboon <159911859+Siriwat-ispb@users.noreply.github.com>
2025-01-09 08:07:14 +0000

    update

--- Main.java
+++ Main.java
@@ -4,19 +4,18 @@ import java.util.Scanner;
 public class Main{
     
     public static void main(String[] args){
-        Scanner input = new Scanner(System.in);
         int Hour,Min,EndHour,EndMin;
         System.out.println("=== Start time ===");
         while(true){
             System.out.println("Enter hour digit (0 - 23) = ");
-            Hour = input.nextInt();
+            Hour = readHour();
             if (Hour >=0 && Hour <=23){
                 break;
             }
         }
         while(true){
             System.out.println("Enter minute digit (0 - 59) = ");
-            Min = input.nextInt();
+            Min = readMinute();
             if (Min >=0 && Min <=59){
                 break;
             }
@@ -25,20 +24,20 @@ public class Main{
         System.out.println("\n=== End time ===");
         while(true){
             System.out.println("Enter hour digit (0 - 23) = ");
-            EndHour = input.nextInt();
+            EndHour = readHour();
             if (EndHour >=0 && EndHour <=23){
                 break;
             }
         }
         while(true){
             System.out.println("Enter minute digit (0 - 59) = ");
-            EndMin = input.nextInt();
+            EndMin = readMinute();
             if (EndMin >=0 && EndMin <=59){
                 break;
             }
         }
 
-        System.out.printf("start time = %02d:%02d, End time = %02d:%02d ",Hour,Min,EndHour,EndMin);
+        System.out.printf("\nstart time = %02d:%02d, End time = %02d:%02d ",Hour,Min,EndHour,EndMin);
         if(EndHour==Hour && EndMin<Min || EndHour<Hour){System.out.println("(tomorrow)");}
         int FinalHour,FinalMin;
         FinalHour = EndHour-Hour;
@@ -47,5 +46,18 @@ public class Main{
         if(FinalMin<0){FinalMin+=60; FinalHour-=1;}
 
         System.out.printf("Duration = %1d hours, %1d minutes\n",FinalHour,FinalMin);
+    }
+
+    public static int readHour(){
+        int Hour;
+        Scanner input = new Scanner(System.in);
+        Hour=input.nextInt();
+        return Hour;
+    }
+    public static int readMinute(){
+        int Min;
+        Scanner input = new Scanner(System.in);
+        Min=input.nextInt();
+        return Min;
     }
 }
